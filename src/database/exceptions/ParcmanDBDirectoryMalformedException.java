abe2b31bcf50a1e24947694aedcc4be27b509a9f
sarimisar <sarimisar@313287c4-dd89-11dd-a893-8d7ce9fbef62>
2008-03-04 08:37:45 +0000

    Aggiunti i javadoc alle Classi Exception in database.exceptions

--- src/database/exceptions/ParcmanDBDirectoryMalformedException.java
+++ src/database/exceptions/ParcmanDBDirectoryMalformedException.java
@@ -1,15 +1,30 @@
 package database.exceptions;
 
+/**
+ * Il path della directory radice non e' valido.
+ * Ad esempio il path punta ad un file oppure la directory
+ * non ha i permessi adeguati per essere usata.
+ * 
+ * @author Parcman Tm
+ */
 public class ParcmanDBDirectoryMalformedException
     extends Exception
 {
     private static final long serialVersionUID = 42L;
 
+    /**
+     * Costruttore.
+     *
+     * @param message Messaggio di errore.
+     */
     public ParcmanDBDirectoryMalformedException (String message)
     {
         super(message);
     }
 
+    /**
+     * Costruttore.
+     */
 	public ParcmanDBDirectoryMalformedException ()
 	{
 
--- src/database/exceptions/ParcmanDBNotCreateException.java
+++ src/database/exceptions/ParcmanDBNotCreateException.java
@@ -1,15 +1,30 @@
 package database.exceptions;
 
+/**
+ * Il database o alcune sue parti non possono essere create.
+ * Ad esempio la directory radice del DB e' priva dei
+ * permessi di scrittura.
+ *
+ * @author Parcman Tm
+ */
 public class ParcmanDBNotCreateException
     extends Exception
 {
     private static final long serialVersionUID = 42L;
 
+    /**
+     * Costruttore.
+     *
+     * @param message Messaggio di errore.
+     */
     public ParcmanDBNotCreateException (String message)
     {
         super(message);
     }
 
+    /**
+     * Costruttore.
+     */
 	public ParcmanDBNotCreateException ()
 	{
 
