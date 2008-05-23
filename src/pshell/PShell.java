3fb504cfc21c25f253b18af55ab17df8a5c07e84
sarimisar <sarimisar@313287c4-dd89-11dd-a893-8d7ce9fbef62>
2008-05-23 09:48:37 +0000

    package pshell aggiunto con primo codice

--- src/pshell/PShell.java
+++ src/pshell/PShell.java
@@ -5,3 +5,57 @@ import java.lang.reflect.*;
 
 import plog.*;
 import pshell.*;
+
+/**
+ * Gestore di Shell
+ *
+ * @author Parcman Tm
+ */
+public class PShell
+{
+    /**
+     * Dati della shell da gestire.
+     */
+    private PShellData shell;
+
+    private Vector<CommandBean> commands;
+
+    /**
+     * Stream di output.
+     */
+    private BufferedOutputStream out;
+
+    /**
+     * Costruttore.
+     *
+     * @param out OutputStream per la Shell
+     * @param shell Dati della Shell
+     */
+    public PShell(BufferedOutputStream out, PShellData shell)
+    {
+        this.out = out;
+        this.shell = shell;
+
+    }
+
+    /**
+     * Stampa il prompt di shell
+     */
+    private void printPrompt()
+    {
+        out.println(shell.getPrompt());
+    }
+}
+
+/**
+ * Bean dati di shell
+ *
+ * @author Parcman Tm
+ */
+private class CommandBean
+{
+    private String name;
+    private String desc;
+    private String help;
+}
+
--- src/pshell/PShellData.java
+++ src/pshell/PShellData.java
@@ -4,3 +4,19 @@ import java.io.*;
 import java.lang.reflect.*;
 
 import plog.*;
+
+/**
+ * Contenitore dati di Shell
+ * 
+ * @author Parcman Tm
+ */
+public interface PShellData
+{
+    /**
+     * Restituisce il prompt di shell.
+     *
+     * @return Stringa contenente il prompt di shell
+     */
+    public String getPrompt();
+}
+
