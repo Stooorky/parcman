03ce72b22cea3d37506ee4ad446495b0e068eb57
sh4d0wblu3 <sh4d0wblu3@313287c4-dd89-11dd-a893-8d7ce9fbef62>
2008-11-12 15:52:10 +0000

    Aggiornato Relazione.txt e corretto errore di scrittura su javadoc

--- src/parcmanclient/ParcmanClient.java
+++ src/parcmanclient/ParcmanClient.java
@@ -95,7 +95,7 @@ public class ParcmanClient
 
     /**
      * Restituisce true se il ParcmanClient ha bisogno di eseguire un
-     * update, cioè l'UpdateList per la versione fornita non e' vuota.
+     * update, cioe' l'UpdateList per la versione fornita non e' vuota.
      *
      * @param versione Versione della SharingList
      * @return true se il ParcmanClient ha bisogno di eseguire un update
