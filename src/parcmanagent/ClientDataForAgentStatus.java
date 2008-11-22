e71e3e05af3d3075c13639ad4c8e086499902ce2
ohjimijimijimi <ohjimijimijimi@313287c4-dd89-11dd-a893-8d7ce9fbef62>
2008-11-22 00:52:24 +0000

    mancava questo

---
+++ src/parcmanagent/ClientDataForAgentStatus.java
@@ -0,0 +1,12 @@
+package parcmanagent;
+
+public class ClientDataForAgentStatus
+{
+	public static final int READY = 0;
+	public static final int IN_UPDATE = 1;
+	public static final int UPDATED = 2;
+	public static final int NO_NEED_UPDATE = 3;
+	public static final int UPDATE_FAILURE = 4;
+	public static final int NOT_AVAILABLE = 5;
+}
+
